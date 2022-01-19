cb6f5e1566c527f6b848260b5fd1ce07c9bafcf4
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2022-01-19 10:58:30 +0000

    naming conventions

--- db/scripts/hackaplanner/dropTable.js
+++ db/scripts/hackaplanner/dropTable.js
@@ -1,7 +1,7 @@
 import query from '../../connection.js';
 
 async function dropTable() {
-  const dropped = await query(`DROP TABLE IF EXISTS users;`);
+  const dropped = await query(`DROP TABLE IF EXISTS hackaplanner;`);
   console.log = ('Dropped Table', dropped);
 }
 
